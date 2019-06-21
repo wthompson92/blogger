6b9ce78e82cb343065a5c89965d61ac9dcd94cc3
username <wnt925@gmail.com>
2019-06-21 13:40:42 -0600

    Add test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :destroy, :edit, :update]
+
   def index
     @articles = Article.all
   end
-  
+
+  def show
+  end
 end
